a8abc53b1a74b9d14f48ad3da196d6d3225a84b3
Syed Muhammad Atif <112538117+bsce22030@users.noreply.github.com>
2024-10-26 23:45:23 +0500

    uff mrgaye

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const faqHeaders = document.querySelectorAll('.faq-header');
-    faqHeaders.forEach(header => {
-        header.addEventListener('click', () => {
-            const faqContent = header.nextElementSibling;
-            faqContent.style.display = faqContent.style.display === 'block' ? 'none' : 'block';
-        });
-    });
-});
